1bc68a22876165cf96e399955d6063eb7c9404d0
Abia Zahir <abiazahir@hotmail.com>
2023-09-16 17:44:35 -0400

    Add files via upload

---
+++ MortgageCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MortgageCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the principal amount:");
+        double principal = scanner.nextDouble();
+
+        System.out.println("Enter the amount of interest rate: ")
+
+    }
+
+
+        
+
+}
